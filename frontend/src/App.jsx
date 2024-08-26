60a8f5dd6ff95916d938d0829fc5f57a239d5ff8
Sema <semamehmetciksm@gmail.com>
2024-08-26 15:37:54 +0300

    3.gün

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
-import "./App.css";
+
+import Categories from "./components/Categories/Categories";
 import Footer from "./components/Layout/Footer/Footer";
 import Header from "./components/Layout/Header/Header";
 import Policy from "./components/Layout/Policy/Policy";
+import Products from "./components/Products/Products";
 import Sliders from "./components/Sliders/Sliders";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
       <Sliders/>
+      <Categories/>
+
+      <Products/>
       <Policy/>
       <Footer />
     </>
