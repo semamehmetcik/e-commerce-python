24e1af40f884017bbe247d2b7391ad1b99687d01
Sema <semamehmetciksm@gmail.com>
2024-08-31 03:03:22 +0300

    5.gün

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import BlogDetailsPage from "./pages/BlogDetailsPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
 
 function App() {
   return (
     <React.Fragment>
       <BlogDetailsPage />
+      <ProductDetailsPage />
     </React.Fragment>
   );
 }
